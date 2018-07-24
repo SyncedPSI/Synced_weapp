f030d0b5d0a0def0adffa9cbfe2a3e656bedeab4
ivyxuan <iloveivyxuan@gmail.com>
2018-07-24 19:39:14 +0800

    Change style of different level of node card

--- src/pages/article/article.js
+++ src/pages/article/article.js
@@ -11,39 +11,6 @@ Page({
       id: "92c7175c-4178-4131-9a35-71d40f443b36",
     },
     isShowComment: false
-    // article: {
-    //   id: "fff5977c-db34-4d59-8ce8-2aaf26fb911a",
-    //   title: "用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验",
-    //   author: {
-    //     id: "05b74512-9c5c-4d1e-a2a7-c5404e94a0d6",
-    //     avatar_url: "../../icons/ic_chatbot_h.svg",
-    //     name: "微胖"
-    //   },
-    //   copyright: "翻译",
-    //   published_at: "2天前",
-    //   content:
-    //     "用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验用神经网络向我比心: 有个当算法工程师的女朋友是这样一种体验",
-    //   related_nodes: [
-    //     {
-    //       node_id: "fff5977c-db34-4d59-8ce8-2aaf26fb911a",
-    //       node_type: "技术",
-    //       zh_name: "激活函数",
-    //       en_name: "Activation Function"
-    //     },
-    //     {
-    //       node_id: "fff5977c-db34-4d59-8ce8-2aaf26fb911a",
-    //       node_type: "技术",
-    //       zh_name: "激活函数",
-    //       en_name: "Activation Function"
-    //     },
-    //     {
-    //       node_id: "fff5977c-db34-4d59-8ce8-2aaf26fb911a",
-    //       node_type: "技术",
-    //       zh_name: "激活函数",
-    //       en_name: "Activation Function"
-    //     }
-    //   ]
-    // }
   },
   closeShowComment: function() {
     this.setData({
