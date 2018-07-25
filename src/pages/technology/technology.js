18e0a9592634204512f9ab6ef6e702f42a8538fb
ivyxuan <iloveivyxuan@gmail.com>
2018-07-25 15:28:38 +0800

    Fix technology level_2 show page

--- src/pages/technology/technology.js
+++ src/pages/technology/technology.js
@@ -15,25 +15,17 @@ Page({
   },
 
   onLoad: function (option) {
-    const $this = this;
     this.setData({
       id: option.id
     });
-    console.log(option.id);
+
     request(`${technologyShow}${option.id}`)
       .then(res => {
         const technology = res.data;
-        console.log(technology);
-        WxParse.wxParse("technology_content", "html", res.data.desc, $this, 5);
-        $this.setData({
+        WxParse.wxParse("technology_content", "html", res.data.desc, this, 5);
+        this.setData({
           technology: technology
         });
       })
-  },
-
-  bindToNodeShow: function (e) {
-    wx.navigateTo({
-      url: `../node/node?id=${e.target.id}`
-    });
   }
 });
