05536606117cff3d5a69c90c18684ae4737a0f3d
Peipei Wong <wangpp1995@gmail.com>
2018-07-26 11:09:32 +0800

    Add search pages data

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -5,18 +5,39 @@ const app = getApp();
 
 Page({
   data: {
-    searchIconUrl: "../../icons/ic_search.svg"
+    searchIconUrl: "../../icons/ic_search.svg",
+    page: 1,
+    articles: [],
+    node: null,
+    hasNextPage: true,
+    isFetching: false,
   },
+  fetchData: function(keywords = '') {
+    this.keywords = keywords;
+    this.switchFetching(true);
+    const { page } = this.data;
+    request(`${searchByKeyword}${keywords}`, {
+      page
+    })
+      .then(({ data }) => {
+        const { articles , hasNextPage } = data;
+        const card_node = data.card_node || null;
+        this.setData({
+          articles: articles,
+          hasNextPage: hasNextPage,
+          node: card_node,
+          page: page + 1,
+          isFetching: false,
+        })
 
-  fetchData: function(keywords) {
-    request(`${searchByKeyword}${keywords}`)
-      .then(res => {
-        // console.log(res);
         this.clearTimer();
       })
       .catch(() => {
         this.clearTimer();
       })
+  },
+  fetchMoreData: function() {
+    this.fetchData(this.keywords);
   },
   search: function(event) {
     this.clearTimer();
@@ -30,8 +51,15 @@ Page({
     this.timer = setTimeout(() => {
       this.fetchData(value);
     }, 2000);
+
+    return value;
   },
   clearTimer: function() {
     clearTimeout(this.timer);
+  },
+  switchFetching: function(status) {
+    this.setData({
+      isFetching: status
+    });
   }
 });
