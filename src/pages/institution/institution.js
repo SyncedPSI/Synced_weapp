cbb3aecdf914879cc1faf186cfc5454bd731146f
ivyxuan <iloveivyxuan@gmail.com>
2018-07-26 14:48:44 +0800

    Add institution show page

--- src/pages/institution/institution.js
+++ src/pages/institution/institution.js
@@ -1,3 +1,32 @@
+import {
+  request
+} from "../../utils/util";
+import {
+  institutionShow
+} from "../../config/api";
+const WxParse = require("../../wxParse/wxParse.js");
+
 const app = getApp();
 
-Page({});
+Page({
+  data: {
+    id: "",
+    institution: {}
+  },
+
+  onLoad: function (option) {
+    this.setData({
+      id: option.id
+    });
+    console.log(this.data.id);
+
+    request(`${institutionShow}${option.id}`)
+      .then(res => {
+        const institution = res.data;
+        WxParse.wxParse("institution_content", "html", res.data.desc, this, 5);
+        this.setData({
+          institution: institution
+        });
+      })
+  }
+});
