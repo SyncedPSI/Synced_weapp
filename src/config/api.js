ce1e6070d3b12d0f8f5afc207b54b7d0fb62d848
ivyxuan <iloveivyxuan@gmail.com>
2018-08-13 01:28:28 +0800

    Fix api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,12 @@
-export const ApiRootUrl = 'https://www.jiqizhixin.com/api/v1/';
+export const ApiRootUrl = 'https://www.jiqizhixin.com/api/v1';
 
 export const timeline =  ApiRootUrl + '/timelines';
 export const articleShow =  ApiRootUrl + '/articles/';
 export const expertShow =  ApiRootUrl + '/experts/';
 export const technologyShow =  ApiRootUrl + '/technologies/';
 export const institutionShow =  ApiRootUrl + '/institutions/';
-export const bindExistAccount = ApiRootUrl + '/bind_exist_account';
-export const login = ApiRootUrl + '/login';
-export const register = ApiRootUrl + '/register';
+export const bindAccount = ApiRootUrl + '/bind_account/';
+export const login = ApiRootUrl + '/login/';
+export const register = ApiRootUrl + '/register/';
 export const searchByKeyword = ApiRootUrl + '/search?type=for_weapp&keywords=';
+export const comments = ApiRootUrl + '/comments';
