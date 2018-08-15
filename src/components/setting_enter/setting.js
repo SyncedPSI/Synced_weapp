6c96565d9ec7852313772139074399a5d5d028b2
Peipei Wong <wangpp1995@gmail.com>
2018-08-15 17:59:16 +0800

    Delay get login status when isLogin is false

--- src/components/setting_enter/setting.js
+++ src/components/setting_enter/setting.js
@@ -4,19 +4,29 @@ Component({
   properties: {
   },
   data: {
+    isHidden: true,
     isLogin: null
   },
   attached: function () {
-    this.setData({
-      isLogin: app.globalData.isLogin
-    });
+    const isLogin = app.globalData.isLogin;
+    if (!isLogin) {
+      setTimeout(() => {
+        this.setData({
+          isLogin: app.globalData.isLogin,
+          isHidden: false
+        });
+      }, 2000);
+    } else {
+      this.setData({
+        isLogin
+      });
+    }
   },
   methods: {
     getUserInfo: function (event) {
       app.login(event.detail.userInfo, () => {
         this.setData({
-          isLogin: true,
-          isShowComment: true
+          isLogin: true
         });
       });
     }
