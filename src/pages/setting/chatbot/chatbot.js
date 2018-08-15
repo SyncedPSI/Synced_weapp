903b1f84dfb8ea105d47afcbffefb434d2a62dfb
Peipei Wong <wangpp1995@gmail.com>
2018-08-15 12:53:02 +0800

    Add setting header to setting index pending chatbot

---
+++ src/pages/setting/chatbot/chatbot.js
@@ -0,0 +1,9 @@
+// import { request, getDateDiff, showErrorToast } from "utils/util";
+// import { articleShow, login } from "config/api";
+
+// const app = getApp();
+
+Page({
+  data: {
+  },
+});
