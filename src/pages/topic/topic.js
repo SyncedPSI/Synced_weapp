366f7c78e9f04fe02239403f067242d484206446
Peipei Wong <wangpp1995@gmail.com>
2018-09-13 11:19:38 +0800

    Add topic page and split article style

---
+++ src/pages/topic/topic.js
@@ -0,0 +1,102 @@
+import { request, getDateDiff } from "utils/util";
+import { articleShow } from "config/api";
+const WxParse = require("wxParse/wxParse.js");
+
+const app = getApp();
+
+Page({
+  data: {
+    navigateTitle: '',
+    id: "",
+    title: "",
+    isFromWeapp: false,
+    isFetching: true,
+    article: {
+      related_nodes: [],
+    },
+    isShowComment: false,
+    isIphoneX: app.globalData.isIphoneX,
+    statusBarHeight: getApp().globalData.systemInfo.statusBarHeight,
+    isLogin: false
+  },
+
+  openComment: function() {
+    this.switchComment(true);
+  },
+
+  closeComment: function() {
+    this.switchComment(false);
+  },
+
+  switchComment: function(status) {
+    this.setData({
+      isShowComment: status
+    });
+  },
+
+  onLoad: function(option) {
+    this.getTitleHeight();
+
+    const { id, title, from } = option;
+    this.setData({
+      id,
+      title,
+      isLogin: app.globalData.isLogin,
+      isFromWeapp: from === "weapp",
+    });
+
+    request(`${articleShow}${option.id}`)
+      .then(res => {
+        const article = res.data;
+        article.published_at = getDateDiff(res.data.published_at);
+        WxParse.wxParse("article_content", "html", res.data.content, this, 5);
+        this.setData({
+          article,
+          isFetching: false
+        });
+      });
+  },
+
+  getTitleHeight: function() {
+    setTimeout(() => {
+      wx.createSelectorQuery().select('#js-article-title').boundingClientRect((rect) => {
+        this.titleHeight = (rect.height + 16);
+      }).exec();
+    }, 300);
+  },
+
+  scroll: function (event) {
+    if (this.titleHeight === undefined) return;
+
+    const { scrollTop } = event.detail;
+    if (scrollTop > this.titleHeight) {
+      this.setNavigationBarTitle(this.data.title);
+    } else {
+      this.setNavigationBarTitle();
+    }
+  },
+
+  setNavigationBarTitle: function(title = '') {
+    this.setData({
+      navigateTitle: title
+    });
+  },
+
+  getUserInfo: function(event) {
+    // event.detail.userInfo
+    app.login(event.detail.userInfo, () => {
+      this.setData({
+        isLogin: true,
+        isShowComment: true
+      });
+    });
+  },
+
+  onShareAppMessage: function() {
+    const { title, id } = this.data;
+    return {
+      title,
+      path: `/pages/article/article?id=${id}&title=${title}&from=weapp`
+    };
+  },
+});
