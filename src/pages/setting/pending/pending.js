9e7f7ecbfd6eba8ad591dfb903052f1f9377d28f
Peipei Wong <wangpp1995@gmail.com>
2018-09-18 18:21:26 +0800

    Add pending page

--- src/pages/setting/pending/pending.js
+++ src/pages/setting/pending/pending.js
@@ -1,3 +1,132 @@
 Page({
-  data: {},
-});
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    articles: [{id: 1}, {id: 2}],
+    startX: 0,
+    startY: 0,
+    maxRightBtnWidth: 80,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  touchStart: function(event) {
+
+    // 开始触摸时 重置所有的删除
+    // 判断是否只有一个触摸点
+    // 记录触摸的坐标
+    const { articles } = this.data;
+    articles.forEach((item) => {
+      item.isOpenBtn = false;
+    });
+    const { clientX, clientY } = event.changedTouches[0];
+    this.setData({
+      articles,
+      startX: clientX,
+      startY: clientY,
+    });
+  },
+
+  touchMove: function(event) {
+    // const index = event.currentTarget.dataset.index,//当前索引
+    // 得到start 的x y
+    // 得到滑动变化的坐标
+    // 滑动角度？？？ angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+    // 判断 左滑还是右滑
+    const index = event.currentTarget.dataset.index;
+    const { clientX, clientY } = event.changedTouches[0];
+    const { startX, startY, articles, maxRightBtnWidth } = this.data;
+    if (clientX > startX) {
+      articles[index].txtStyle = '';
+      this.setData({
+        articles,
+      });
+      return; //右滑
+    }
+
+    let distance = clientX - startX;
+    if (maxRightBtnWidth < -distance) {
+      distance = -maxRightBtnWidth;
+    }
+    articles[index].txtStyle = `left: ${distance}px`;
+    this.setData({
+      articles,
+    });
+  },
+
+  touchEnd: function(event) {
+    // 得到结束的触摸点位置
+    // 滑动距离和删除按钮的长度相比 是显示 还是不显示
+    const index = event.currentTarget.dataset.index;
+    const { clientX, clientY } = event.changedTouches[0];
+    const { startX, startY, articles, maxRightBtnWidth } = this.data;
+    const distance = clientX - startX;
+    articles[index].txtStyle = '';
+
+    if (maxRightBtnWidth < -distance) {
+      articles[index].isOpenBtn = true;
+    }
+
+    this.setData({
+      startX: 0,
+      startY: 0,
+      articles,
+    });
+  },
+})
