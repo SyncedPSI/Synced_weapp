e303f15ca480fbf7e6827882bd27359ce1a82945
Peipei Wong <wangpp1995@gmail.com>
2018-11-14 16:53:43 +0800

    Add trend page

--- src/pages/trend/trend.js
+++ src/pages/trend/trend.js
@@ -1,3 +1,20 @@
-Page({
+// import { request } from "utils/util";
 
+Page({
+  data: {
+    statusBarHeight: getApp().globalData.systemInfo.statusBarHeight,
+    slide: [1, 2, 3, 4, 5],
+    hotList: [1, 2, 3, 4, 5, 6, 7, 8]
+  },
+  onLoad: function () {
+    // this.dailyPage = 1;
+    // this.getDailyList();
+  },
+
+  onShareAppMessage: function() {
+    return {
+      title: '机器之心',
+      path: '/pages/trend/trend'
+    };
+  },
 })
