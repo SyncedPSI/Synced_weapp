f29503d0cd1caae408b2ba46374271f05fe74bc4
Peipei Wong <wangpp1995@gmail.com>
2018-11-15 15:48:17 +0800

    Adjust banner in trend page

--- src/pages/trend/trend.js
+++ src/pages/trend/trend.js
@@ -4,11 +4,17 @@ Page({
   data: {
     statusBarHeight: getApp().globalData.systemInfo.statusBarHeight,
     slide: [1, 2, 3, 4, 5],
-    hotList: [1, 2, 3, 4, 5, 6, 7, 8]
+    hotList: [1, 2, 3, 4, 5, 6, 7, 8],
+    currentBannerIndex: 0,
   },
   onLoad: function () {
     // this.dailyPage = 1;
     // this.getDailyList();
+  },
+  bannerChange: function (event) {
+    this.setData({
+      currentBannerIndex: event.detail.current
+    });
   },
 
   onShareAppMessage: function() {
