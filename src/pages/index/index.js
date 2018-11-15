ee488667faaa11c2b5a6bd8064e27eab80c7fcf0
Peipei Wong <wangpp1995@gmail.com>
2018-11-15 15:56:03 +0800

    Rename index to category, trend to index

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,73 +1,26 @@
-import { request, getDateDiff } from "utils/util";
-import { articles } from "config/api";
+// import { request } from "utils/util";
 
 Page({
   data: {
-    articleList: [],
     statusBarHeight: getApp().globalData.systemInfo.statusBarHeight,
-    scrollTop: 0,
-    activeCategory: 'all',
-    category: [
-      { en: 'all', zh: '全部' },
-      { en: 'industry', zh: '产业' },
-      { en: 'practice', zh: '工程' },
-      { en: 'theory', zh: '理论' },
-      { en: 'basic', zh: '入门' },
-    ]
+    slide: [1, 2, 3, 4, 5],
+    hotList: [1, 2, 3, 4, 5, 6, 7, 8],
+    currentBannerIndex: 0,
   },
-
-  onLoad: function() {
-    this.articlePage = 1;
-    this.reportPage = 1;
-    this.fixNavTop = 176;
-
-    this.getArticleList();
+  onLoad: function () {
+    // this.dailyPage = 1;
+    // this.getDailyList();
   },
-  switchCategory: function(event) {
+  bannerChange: function (event) {
     this.setData({
-      activeCategory: event.target.dataset.type,
-      scrollTop: 0,
-      articleList: [],
-    }, () => {
-      this.articlePage = 1;
-      this.getArticleList();
+      currentBannerIndex: event.detail.current
     });
   },
-  getArticleList: function () {
-    const { activeCategory } = this.data;
-    const queryCategory = activeCategory === 'all' ? '' : `&category=${activeCategory}`;
-    return request(`${articles}?page=${this.articlePage}${queryCategory}`)
-      .then(res => {
-        this.articlePage += 1;
-        const { articleList } = this.data;
-        const newList = res.data;
-        newList.forEach(item => {
-          item.published_at = getDateDiff(item.published_at);
-        });
 
-        this.setData({
-          articleList: [...articleList, ...newList],
-        });
-      });
-  },
-  fetchMoreData: function () {
-    this.getArticleList();
-  },
-  // onPullDownRefresh: function () {
-  //   wx.showNavigationBarLoading();
-
-  //   this.page = 1;
-  //   this.getList(true)
-  //     .catch(() => {})
-  //     .then(() => {
-  //       wx.hideNavigationBarLoading();
-  //       wx.stopPullDownRefresh();
-  //     });
-  // },
   onShareAppMessage: function() {
     return {
       title: '机器之心',
-      path: '/pages/index/index?from=weapp'
+      path: '/pages/index/index'
     };
   },
-});
+})
