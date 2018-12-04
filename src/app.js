1c500da65ffe93cbaf34d30ad958d026ef4556ad
Peipei Wong <wangpp1995@gmail.com>
2018-12-04 14:16:16 +0800

    Add check version update func

--- src/app.js
+++ src/app.js
@@ -17,6 +17,26 @@ App({
     this.getCookies();
   },
 
+  checkUpdate: function() {
+    const updateManager = wx.getUpdateManager();
+
+    updateManager.onCheckForUpdate(function (res) {
+      if (res.hasUpdate) {
+        updateManager.onUpdateReady(function () {
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用？',
+            success: function (res) {
+              if (res.confirm) {
+                updateManager.applyUpdate();
+              }
+            }
+          })
+        })
+      }
+    })
+  },
+
   getCookies: function() {
     request(getAhoyTokens, {}, 'POST')
       .then((res) => {
