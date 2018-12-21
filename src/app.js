2aa23c6754dc87c5cf8a3ba8eda0f840f1dc05e1
Peipei Wong <wangpp1995@gmail.com>
2018-12-21 17:45:35 +0800

    Comment gio

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import { request, showTipToast } from "utils/util";
 import { login, getAhoyTokens } from "config/api";
-import gio from "utils/gio-minp.js";
+// import gio from "utils/gio-minp.js";
 
-gio('init', 'ab6599720580bdfd', 'wxf424e2f3e2f94500', { version: '1.0.3', followShare: true });
+// gio('init', 'ab6599720580bdfd', 'wxf424e2f3e2f94500', { version: '1.0.3', followShare: true });
 
 App({
   globalData: {
@@ -119,7 +119,7 @@ App({
     showTipToast('正在登录', 'loading');
     var encryptedData = '';
     var iv = '';
-    gio('setVisitor', userInfo);
+    // gio('setVisitor', userInfo);
     wx.setStorage({
       key: 'userInfo',
       data: userInfo
