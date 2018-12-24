5f27cda87b425dde2f8daf2724a230d29607bd25
Peipei Wong <wangpp1995@gmail.com>
2018-12-24 10:57:50 +0800

    Add track to related_nodes in graph brief page

--- src/components/related_nodes/nodes.js
+++ src/components/related_nodes/nodes.js
@@ -17,4 +17,13 @@ Component({
       value: false
     }
   },
+  methods: {
+    track: function(event) {
+      const { query } = event.currentTarget.dataset;
+      getApp().td_app_sdk.event({
+        id: `related_node_in_graph_with_tap`,
+        label: `tap_query_is_${query}`,
+      });
+    },
+  }
 })
