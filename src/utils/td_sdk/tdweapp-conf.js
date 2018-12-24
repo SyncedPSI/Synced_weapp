cab7d688949e7cf286512337e93211fd56983b0a
Peipei Wong <wangpp1995@gmail.com>
2018-12-24 10:35:35 +0800

    Add td_weapp and remove gio

---
+++ src/utils/td_sdk/tdweapp-conf.js
@@ -0,0 +1,10 @@
+exports.config = {
+  appkey: '6BAC5E835B2C46CB9FDB0039BDDDB952',
+  appName: 'synced weapp',
+  versionName: 'v1.1.2',
+  versionCode: '1.1.2',
+  wxAppid: 'wxf424e2f3e2f94500',
+  getLocation: false, // 默认不获取用户位置
+  autoOnPullDownRefresh: false, // 默认不统计下拉刷新数据
+  autoOnReachBottom: false // 默认不统计页面触底数据
+};
