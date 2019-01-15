6302b0574bfdcfcb6d385ee94efa9cba7924ccd5
Peipei Wong <wangpp1995@gmail.com>
2019-01-15 13:25:12 +0800

    Add share action to web_view page

--- src/pages/web_view/web_view.js
+++ src/pages/web_view/web_view.js
@@ -8,5 +8,11 @@ Page({
     this.setData({
       url,
     })
-  }
+  },
+  onShareAppMessage: function() {
+    const { url } = this.data;
+    return {
+      path: `/pages/web_view/web_view?url=${url}`
+    };
+  },
 })
